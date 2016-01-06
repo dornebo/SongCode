3d6367a6f4872d213ae5c665c5c45516fb4f7462
Kevin <song.vip777@gmail.com>
2016-01-06 16:50:22 +0800

    wechat服务类

--- BaeWechat/src/main/java/com/chat/BaeWechat/controller/wechat/WechatController.java
+++ BaeWechat/src/main/java/com/chat/BaeWechat/controller/wechat/WechatController.java
@@ -1,5 +1,39 @@
 package com.chat.BaeWechat.controller.wechat;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.chat.BaeWechat.service.WechatService;
+
+
+@Controller
+@RequestMapping(value = "wechat")
 public class WechatController {
+	
+	@Resource(name = "wechatService")
+	private WechatService wechatService;
+
+	/**
+	 * 
+	 * @Title: callback 
+	 * @Description: TODO
+	 * @param @param request
+	 * @param @return
+	 * @param @throws Exception   
+	 * @return String    
+	 * @url http://www.0412.party/wechat/callback.do
+	 * @throws
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/callback", produces = "text/xml;charset=UTF-8")
+	public void callback(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	        String result = wechatService.execute();
+	        response.getOutputStream().write(result.getBytes());
+	}
 
 }
